d62c828044cfc14cb42ccfba01e7e85962ab68ae
jeets15 <149242994+jeets15@users.noreply.github.com>
2025-02-26 14:18:31 +0000

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
-        userPoolId: 'us-east-1_a5NAsSsDg', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '5tkb1dv3gb7c8cb2tnuqo4h8lb', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'us-east-1_R4Y9t2xi7', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: 'ajrb0q201c0tpgirqku0k5nb0', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'us-east-1' // e.g. us-east-2
     },
     api: {
